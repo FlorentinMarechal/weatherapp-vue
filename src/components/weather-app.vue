556928e336e6e7ad039423a49a792d5345a50d08
Florentin Marechal <marechalflorentin@gmail.com>
2023-05-03 03:54:03 +0200

    netoyage + début

---
+++ src/components/weather-app.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="weather-container">
+        <div class="weather-wrap">
+            <div class="search-box">
+                <input type="text" placeholder="Search..." class="search-bar" v-model="query" v-on:keypress="fetchWeather" />
+            </div>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
